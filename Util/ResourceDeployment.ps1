f73bad80104d57987c718cc5a2c25ee1765bf8f1
SFetsenko <mr.fec88@gmail.com>
2021-05-25 08:39:37 +0300

    SQL

--- Util/ResourceDeployment.ps1
+++ Util/ResourceDeployment.ps1
@@ -1,4 +1,4 @@
 #New-AzResourceGroupDeployment -Name "new-vnet" -ResourceGroupName "SKILLUP-RG" -TemplateFile .\vnet.json
 #New-AzResourceGroupDeployment -Name "new-nsg" -ResourceGroupName "SKILLUP-RG" -TemplateFile .\nsg.json
 Connect-AzAccount
-New-AzResourceGroupDeployment -Name "new-vm" -ResourceGroupName "SKILLUP-RG" -TemplateFile .\VirtualMachine\vm.json -TemplateParameterFile .\VirtualMachine\vm.parameters.json
+New-AzResourceGroupDeployment -Name "new-sql" -ResourceGroupName "SKILLUP-RG" -TemplateFile .\AzureSQL\AzureSQLServer.json -TemplateParameterFile .\AzureSQL\AzureSQLServer.parameters.json
